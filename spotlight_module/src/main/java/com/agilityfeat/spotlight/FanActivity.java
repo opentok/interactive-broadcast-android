d4e3029d9e5c8f2803bb26878946c0a050bb337e
German <german.goldenstein@gmail.com>
2015-12-22 12:21:41 -0300

    Fix onStreamCreated

--- spotlight_module/src/main/java/com/agilityfeat/spotlight/FanActivity.java
+++ spotlight_module/src/main/java/com/agilityfeat/spotlight/FanActivity.java
@@ -930,7 +930,7 @@ public class FanActivity extends AppCompatActivity implements WebServiceCoordina
                 mTestingOnStage = true;
                 testStreamConnectionQuality(mCelebirtyStream);
             } else {
-                mTestingOnStage = false
+                mTestingOnStage = false;
                 testStreamConnectionQuality(stream);
             }
         } else {
